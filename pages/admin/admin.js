fbd34e98cca46d1128dc160357294047d8097c0a
Hanie1994 <rahmati_hanie@yahoo.com>
2016-05-27 14:50:28 +0430

    Admin Menu

--- pages/admin/admin.js
+++ pages/admin/admin.js
@@ -5,11 +5,12 @@ angular.module('stockApp').controller('AdminCtrl', ['$scope', '$http', function(
     $scope.menu = [
         {id:"DepositMenu",      title:"ویرایش موجودی", image:imageDir+"cash.png",   name:"ویرایش موجودی"},
         {id:"StatusMenu",       title:"بازار",         image:imageDir+"status.png", name:"نبض بازار"},
-        {id:"ConfigMenu",       title:"تنظیمات",       image:imageDir+"config.png", name:"تنظیمات"},
-        {id:"OrderMenu",        title:"خرید و فروش",   image:imageDir+"order.png",  name:"تایید نماد"},
+        {id:"PersonalMenu",       title:"پروفایل",       image:imageDir+"config.png", name:"پروفایل"},
+        {id:"OrderMenu",        title:"خرید و فروش",   image:imageDir+"order.png",  name:"تایید نمادها"},
         {id:"ReportMenu",       title:"درخواست ها",    image:imageDir+"report.png", name:"مدیریت درخواست ها"},
-        {id:"ReportMenu",       title:"درخواست ها",    image:imageDir+"report.png", name:"دیگر کاربران"},
-        {id:"BackupMenu",       title:"پشتیبان",       image:imageDir+"backup.png", name:"backup"}
+        {id:"OthersMenu",       title:"درخواست ها",    image:imageDir+"report.png", name:"دیگر کاربران"},
+        {id:"BackupMenu",       title:"پشتیبان",       image:imageDir+"backup.png", name:"پشتیبانی"},
+        {id:"FilterMenu",       title:"فیلتر",       image:imageDir+"backup.png", name:"فیلتر"}
     ];
 
     $scope.isActive = function(i){
