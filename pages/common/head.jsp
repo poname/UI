6f35bd6ce0fe9ef06d4fcdb7707eac1603d7dd5f
Dani <@>
2016-05-27 02:16:23 -0700

    Customer

---
+++ pages/common/head.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dani
+  Date: 4/7/2016
+  Time: 2:39 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<head>
+    <title>${param.pageTitle}</title>
+
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../css/custom.css">
+
+    <script src="../js/jquery.js" type="text/javascript"></script>
+    <script src="../js/bootstrap.min.js"></script>
+</head>
--- pages/customer/customer.js
+++ pages/customer/customer.js
@@ -1,11 +1,15 @@
 angular.module('stockApp').controller('CustomerCtrl', ['$scope', '$http', function($scope, $http){
 
+
+    $scope.activeIndex = 'PersonalInfo' ;
     var imageDir = "asset/images/" ;
     $scope.menu = [
-        {id:"DepositMenu",      title:"ویرایش موجودی", image:imageDir+"cash.png",   name:"ویرایش موجودی"},
-        {id:"OrderMenu",        title:"خرید و فروش",   image:imageDir+"order.png",  name:"ثبت سفارش"},
-        {id:"StatusMenu",       title:"بازار",         image:imageDir+"status.png", name:"نبض بازار"},
-        {id:"ConfigMenu",       title:"تنظیمات",       image:imageDir+"config.png", name:"تنظیمات"}
+        {id:"PersonalInfo",      title:"اطلاعات شخصی", image:imageDir+"cash.png",   name:"پروفایل"},
+        {id:"Orders",        title:"",   image:imageDir+"order.png",  name:"سفارش ها"},
+        {id:"SymbolList",        title:"",   image:imageDir+"order.png",  name:"لیست نمادها"},
+        {id:"MarketInfo",       title:"بازار",         image:imageDir+"status.png", name:"نبض بازار"},
+        {id:"ShareBasket",       title:"",       image:imageDir+"config.png", name:"سبد سهام"},
+        {id:"Credits",       title:"",       image:imageDir+"config.png", name:"امور مالی"}
     ];
 
     $scope.isActive = function(i){
@@ -13,7 +17,10 @@ angular.module('stockApp').controller('CustomerCtrl', ['$scope', '$http', functi
     };
 
     $scope.goToPage = function(i){
-        //alert($scope.activeIndex + '\t' + i);
         $scope.activeIndex = i ;
     };
+
+    $scope.userInfo = {id:51, name:'غلام'};
+
+    $scope.welcome = $scope.userInfo.name + " عزیز خوش آمدید " ;
 }]);
